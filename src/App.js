e15c412202975794b6a1e214182ac57aae665d09
Lee jongmin <no3796@naver.com>
2020-09-18 16:19:45 +0900

    Edit readme.md, app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+function Food({fav}){
+return <h3>I like {fav}</h3>;
+}
+
 function App() {
-  //return  <div className="App" />;
-  return <div>Hello</div>;
+  return (
+  <div>
+    <h1>Hello</h1>
+    <Food fav="김치" />
+    <Food fav="피자" />
+    </div>
+    );
 }
 
 export default App;
