92260f19fbabd58cdd803ebedbce4362ad3053c3
Lee jongmin <no3796@naver.com>
2020-11-27 01:13:04 +0900

    Updates

---
+++ precache-manifest.e214f42996b3f3af43b2912d1baaa5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d623d4b8b126706c5f6d90c4ed3718",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f7986e25b6b02f11b033",
+    "url": "/movie_app_2020/static/css/main.dbd58ed2.chunk.css"
+  },
+  {
+    "revision": "e2db8dbeff0daf97e999",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7986e25b6b02f11b033",
+    "url": "/movie_app_2020/static/js/main.e5ea1f2a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  },
+  {
+    "revision": "0b50670d4facc0363b3d249e8d72e541",
+    "url": "/movie_app_2020/static/media/star.0b50670d.jpg"
+  }
+]);
